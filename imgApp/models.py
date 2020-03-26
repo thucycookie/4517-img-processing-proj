f26d66e842d985939ab9e6ae8e7da833565a12ee
tug98850 <tug98850@temple.edu>
2020-03-26 19:54:37 -0400

    Added comments for content_file_name function and both of the classes.

--- imgApp/models.py
+++ imgApp/models.py
@@ -10,17 +10,20 @@ import os
 # rename the file as the input's name
 # not the original name of the file
 def content_file_name(instance, filename):
+
+    # save the image on a local folder
     ext = filename.split('.')[-1]
     filename = "%s" % (instance.name)
-    return os.path.join('images', filename + "." + ext)
+    return os.path.join('images', "{}.{}".format(filename, ext))
 
-
+# model for uploading images view
 class ImageModel(models.Model): 
         name = models.CharField(max_length=50)
         preset_gray_or_poster_or_solar_or_none = models.CharField(max_length=50) 
         Main_Img = models.FileField(upload_to=content_file_name)
         ext = models.CharField(max_length=50)
 
+# model for downloading images view
 class DownloadImageModel(models.Model):
         name_To_Download = models.CharField(max_length=50)
         preset_gray_or_poster_or_solar_or_none = models.CharField(max_length=50)
